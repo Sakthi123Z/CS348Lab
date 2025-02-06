cdad1839017d4d364634e8495f8e7dc73e1c965e
Sakthivel Sivasubramanian <sakthivelsivasubramanian@MacBook-Pro-536.local>
2025-02-06 12:46:05 -0600

    Replace the hello world program with the sample C program

--- hello.c
+++ hello.c
@@ -1,6 +1,28 @@
-# include <stdio.h> 
+# include <stdio.h>
+# include <string.h>
 int main() 
 {
-    printf("bye world/n");
+    char str[100];
+    int i , len;
+
+    printf("Please Type string ... ");
+    fgets(str , sizeof(str) , stdin);
+
+    len = strlen(str);
+
+    for (i=0 ; i < len;i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 'a' + 'A';
+        }
+    }
+    printf("The Uppercase string is ... %s\n" , str);
+    
+    for (i = 0 , len = strlen(str) ; i<len/2;i++) {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
+    printf("The Reversed string is ... %s\n" , str);
+
     return 0;
 }
